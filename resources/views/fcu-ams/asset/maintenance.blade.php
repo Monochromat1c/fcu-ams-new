b4562e6339ca551b9f4767310f2806b2bf6f216e
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2024-12-12 16:38:33 +0800

    Modified the dropdown option when clicking the profile in the navbar.

--- resources/views/fcu-ams/asset/maintenance.blade.php
+++ resources/views/fcu-ams/asset/maintenance.blade.php
@@ -7,7 +7,70 @@
     <div class="content min-h-screen bg-slate-100 col-span-5">
         <!-- Enhanced Header -->
         <nav class="bg-white flex justify-between py-3 px-4 m-3 shadow-md rounded-md">
-            <div></div>
+            <div class="relative" x-data="{ open: false }">
+                <button @click="open = !open" class="invisible flex gap-3 focus:outline-none" style="min-width:100px;">
+                    <div>
+                         @if(auth()->user()->profile_picture)
+                            <img src="{{ asset(auth()->user()->profile_picture) }}" alt="User Profile"
+                                class="w-14 h-14 object-cover bg-no-repeat rounded-full mx-auto">
+                        @else
+                            <img src="{{ asset('profile/defaultProfile.png') }}" alt="Default Image"
+                                class="w-14 h-14 object-cover bg-no-repeat rounded-full mx-auto">
+                        @endif
+                    </div>
+                     
+                </button>
+                <div x-show="open" 
+                    @click.away="open = false"
+                    class="absolute right-0 mt-4 border-2 border-gray-400 w-72 bg-white rounded-lg shadow-lg border border-gray-100 z-50">
+                    <div class="p-4 border-b border-gray-100 r rounded-t-lg bg-gray-200">
+                        <div class="flex items-start">
+                            <div class="flex-shrink-0">
+                                @if(auth()->user()->profile_picture)
+                                    <img src="{{ asset(auth()->user()->profile_picture) }}" alt="User Profile"
+                                        class="w-12 h-12 rounded-full object-cover">
+                                @else
+                                    <img src="{{ asset('profile/defaultProfile.png') }}" alt="Default Profile"
+                                        class="w-12 h-12 rounded-full object-cover">
+                                @endif
+                            </div>
+                            <div class="ml-3 flex-grow">
+                                <div class="font-medium text-base">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</div>
+                                <div class="text-sm text-gray-500">{{ auth()->user()->email }}</div>
+                            </div>
+                            <a href="{{ route('profile.index') }}" class="ml-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex items-center border-b-2 border-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                        </svg>
+                        <p class="role">
+                            Role
+                        </p>
+                        <p class="ml-auto">{{ auth()->user()->role->role }}</p>
+                    </div>
+                    <div class="flex items-center border-b-2 border-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                        </svg>
+                        <span>Username</span>
+                        <span class="ml-auto">{{ auth()->user()->username }}</span>
+                    </div>
+                    <button onclick="document.getElementById('logout-modal').classList.toggle('hidden')"
+                        class="flex items-center px-4 py-2 text-red-500 hover:bg-gray-100 w-full text-left">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
+                        </svg>
+                        <span>Logout</span>
+                    </button>
+                </div>
+            </div>
             <h1 class="my-auto text-3xl">Maintenance</h1>
             <div class="relative" x-data="{ open: false }">
                 <button @click="open = !open" class="flex gap-3 focus:outline-none" style="min-width:100px;">
@@ -20,28 +83,56 @@
                                 class="w-14 h-14 object-cover bg-no-repeat rounded-full mx-auto">
                         @endif
                     </div>
-                    <p class="my-auto">
-                        {{ (auth()->user() ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'N/A') }}
-                    </p>
+                     
                 </button>
                 <div x-show="open" 
                     @click.away="open = false"
-                    class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg border border-gray-100 z-50">
-                    <a href="{{ route('profile.index') }}"
-                        class="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100">
+                    class="absolute right-0 mt-4 border-2 border-gray-400 w-72 bg-white rounded-lg shadow-lg border border-gray-100 z-50">
+                    <div class="p-4 border-b border-gray-100 r rounded-t-lg bg-gray-200">
+                        <div class="flex items-start">
+                            <div class="flex-shrink-0">
+                                @if(auth()->user()->profile_picture)
+                                    <img src="{{ asset(auth()->user()->profile_picture) }}" alt="User Profile"
+                                        class="w-12 h-12 rounded-full object-cover">
+                                @else
+                                    <img src="{{ asset('profile/defaultProfile.png') }}" alt="Default Profile"
+                                        class="w-12 h-12 rounded-full object-cover">
+                                @endif
+                            </div>
+                            <div class="ml-3 flex-grow">
+                                <div class="font-medium text-base">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</div>
+                                <div class="text-sm text-gray-500">{{ auth()->user()->email }}</div>
+                            </div>
+                            <a href="{{ route('profile.index') }}" class="ml-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex items-center border-b-2 border-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                        </svg>
+                        <p class="role">
+                            Role
+                        </p>
+                        <p class="ml-auto">{{ auth()->user()->role->role }}</p>
+                    </div>
+                    <div class="flex items-center border-b-2 border-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-100">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
                         </svg>
-                        Profile
-                    </a>
+                        <span>Username</span>
+                        <span class="ml-auto">{{ auth()->user()->username }}</span>
+                    </div>
                     <button onclick="document.getElementById('logout-modal').classList.toggle('hidden')"
-                        class="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 w-full text-left">
+                        class="flex items-center px-4 py-2 text-red-500 hover:bg-gray-100 w-full text-left">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd"
-                                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                                clip-rule="evenodd" />
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
                         </svg>
-                        Logout
+                        <span>Logout</span>
                     </button>
                 </div>
             </div>
