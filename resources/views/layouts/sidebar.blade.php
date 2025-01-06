3ffb080735bb2428cacdf0403fb07532356b6168
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-01-06 20:05:16 +0800

    Wrap sidebar branding elements in a link to the dashboard for improved navigation

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,9 +12,11 @@
 
 <div class="sidebar flex-grow flex flex-col overflow-y-auto">
     <div class="flex flex-col p-1 mx-auto">
-        <img class="fcu-icon mb-1 mx-auto" src="/img/login/fcu-icon.png" alt="" srcset="">
-        <h1 class="text-blue-900 text-center text-3xl">FCU</h1>
-        <h2 class="text-blue-900 text-center text-xl">Asset Management System</h2>
+        <a href="{{ route('dashboard') }}">
+            <img class="fcu-icon mb-1 mx-auto" src="/img/login/fcu-icon.png" alt="" srcset="">
+            <h1 class="text-blue-900 text-center text-3xl">FCU</h1>
+            <h2 class="text-blue-900 text-center text-xl">Asset Management System</h2>
+        </a>
     </div>
     <!-- <div class="bg-blue-900 break-line"></div> -->
     <div class=" break-line"></div>
