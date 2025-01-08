70f6a653d2c4ca40140a2e7bee384a66e34a9818
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-01-08 17:07:58 +0800

    Refactor navigation links in supply request details view
    - Updated back navigation links in the supply request details view to use JavaScript for improved functionality, allowing users to return to the previous page without relying on server-side URL generation.
    - Enhanced user experience by ensuring consistent behavior across navigation elements, streamlining the overall interface.

--- resources/views/fcu-ams/inventory/supplyRequestDetails.blade.php
+++ resources/views/fcu-ams/inventory/supplyRequestDetails.blade.php
@@ -11,7 +11,7 @@
             <div class="px-4 sm:px-6 lg:px-8 py-6">
                 <div class="flex justify-between items-center">
                     <div class="flex items-center justify-between w-full">
-                        <a href="{{ url()->previous() }}"
+                        <a href="javascript:void(0);" onclick="history.back();"
                         class="mr-4 hover:bg-gray-100 my-auto p-2 rounded-full transition">
                             <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -20,7 +20,7 @@
                             </svg>
                         </a>
                         <h1 class="text-2xl font-semibold text-gray-900">Supply Request Details</h1>
-                        <a href="{{ url()->previous() }}"
+                        <a href="javascript:void(0);" onclick="history.back();"
                             class="mr-4 hover:bg-gray-100 my-auto p-2 rounded-full transition invisible">
                             <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
