a9860e3f82694c4582f8cecbceeb76089e5f896c
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-01-11 18:17:44 +0800

    Removed the 'Users' link in the sidebar for users with manager role.

--- resources/views/layouts/sidebar-links.blade.php
+++ resources/views/layouts/sidebar-links.blade.php
@@ -260,15 +260,6 @@
             Suppliers</a>
     </div>
 </div>
-<a href="{{ route('user.index') }}"
-    class="flex items-center px-4 py-2 text-gray-700  hover:bg-slate-100
-    {{ Request::is('user/index') ? 'bg-slate-200 hover:bg-slate-200' : '' }}">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-        <path
-            d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-    </svg>
-    Users
-</a>
 @endif
 
 
