6ae51e1c10c86ed38bc97a63cd2688218512b354
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-01-30 09:37:31 +0800

    Add user profile dropdown menu with profile details and logout option in notification page for departments

--- resources/views/fcu-ams/dashboard.blade.php
+++ resources/views/fcu-ams/dashboard.blade.php
@@ -114,7 +114,7 @@
                         <button onclick="document.getElementById('logout-modal').classList.toggle('hidden')"
                             class="flex items-center px-4 py-3.5 text-red-600 hover:bg-red-50 w-full text-left transition-colors duration-200">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7a4 4 0 01-4 4v1a3 3 0 003 3h6a3 3 0 003-3v-1a4 4 0 01-4-4z" />
+                                <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
                             </svg>
                             <span class="font-medium">Logout</span>
                         </button>
