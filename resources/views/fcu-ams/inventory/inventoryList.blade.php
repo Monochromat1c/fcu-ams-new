258b6b407ea2c7326d1f716cf1c085178bde9f0a
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-02-03 14:10:36 +0800

    Update inventory search placeholder and search icon
    - Change search input placeholder from "Search by Unique Tag" to "Search for supplies"
    - Simplify search icon SVG path for cleaner visual representation

--- resources/views/fcu-ams/inventory/inventoryList.blade.php
+++ resources/views/fcu-ams/inventory/inventoryList.blade.php
@@ -304,11 +304,11 @@
                         <div class="relative flex-1">
                             <input type="text" name="search" value="{{ request('search') }}"
                                 class="w-full rounded-md border-0 py-2 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                                placeholder="Search by Unique Tag..." id="searchInput">
+                                placeholder="Search for supplies..." id="searchInput">
                             <div class="absolute inset-y-0 right-0 flex items-center pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
                                     class="w-5 h-5 text-gray-400">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607z" />
                                 </svg>
                             </div>
                         </div>
