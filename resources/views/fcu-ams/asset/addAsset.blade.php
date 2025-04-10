d200b8ca085a977bd14f2974b245f6f27266a806
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-04-10 10:04:23 +0800

    ...

--- resources/views/fcu-ams/asset/addAsset.blade.php
+++ resources/views/fcu-ams/asset/addAsset.blade.php
@@ -8,7 +8,7 @@
     </div>
     <div :class="{ 'col-span-5': sidebarOpen, 'col-span-6': !sidebarOpen }" class="bg-slate-200 content min-h-screen">
         <!-- Header -->
-        <nav class="bg-white flex justify-between py-3 px-4 m-3 2xl:max-w-7xl 2xl:mx-auto shadow-md rounded-md">
+        <nav class="bg-white flex justify-between py-6 px-4 m-3 2xl:max-w-7xl 2xl:mx-auto shadow-md rounded-md">
             <button @click="sidebarOpen = !sidebarOpen" class="p-2 rounded-lg hover:bg-gray-100 transition-colors duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
